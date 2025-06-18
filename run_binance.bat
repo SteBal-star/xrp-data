81009f4503b9bfe4731f0a5028d75e894debc14d
Sydney <sydney@example.com>
2025-06-18 00:23:13 -0400

    Update CSV data

---
+++ run_binance.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /d "C:\Users\That's Me\Desktop\xrp-data"
+echo ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+echo ▶ Lancement du script main.py...
+echo ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+python main.py
+echo ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+echo ▶ Script terminé.
+timeout /t 5 >nul
+exit
